9bf594d3582b2dc275f150c59cb5dab3b081b655
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:27:18 +0200

    created example of code

--- TouristRental/code/code_for_using_dataModel.TourismDestinations_TouristRental.py
+++ TouristRental/code/code_for_using_dataModel.TourismDestinations_TouristRental.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "TouristRental"
 subject = "dataModel.TourismDestinations"
-rentalSource = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:Vehicle:VC:0001'}"
+rentalSource = "urn:ngsi-ld:Vehicle:VC:0001"
 attribute = "rentalSource"
 value = rentalSource
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-startDate = "{'type': 'Property', 'value': {'@type': 'date-time', '@value': '2018-09-21T12:00:00Z'}}"
+startDate = "2018-09-21T12:00:00Z"
 attribute = "startDate"
 value = startDate
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-endDate = "{'type': 'Property', 'value': {'@type': 'date-time', '@value': '2018-09-21T12:00:00Z'}}"
+endDate = "2018-09-21T12:00:00Z"
 attribute = "endDate"
 value = endDate
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-leadTourist = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:TouristProfile:VC:0001'}"
+leadTourist = "urn:ngsi-ld:TouristProfile:VC:0001"
 attribute = "leadTourist"
 value = leadTourist
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
